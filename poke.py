422fddd75df82e2c092970c1a2844dd5e63a8c06
vihar08 <33600458+vihar08@users.noreply.github.com>
2017-11-12 20:52:58 +0530

    Add files via upload

---
+++ poke.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import requests
+import json
+import webbrowser
+import speech_recognition as sr
+
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+try:
+    name = r.recognize_google(audio)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio") 
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+print (name)
+
+def getpokeimage(text):
+    #pok = raw_input("Name :")
+    try:
+        url = 'https://pokeapi.co/api/v2/pokemon/'+ text
+        resp = requests.get(url)
+        data = json.loads(resp.content.decode())
+    except requests.HTTPError as e:
+        code  = e.response.status_code
+        raise("The response from the API is :",code)
+    
+    num = str(data.get("id"))
+    img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+ num +".png"
+    webbrowser.open(img)
+
+
+getpokeimage(name) 
